171e1d90072f39bd292cd694c3ae5e35efdf71b6
vpali91 <44944542+vpali91@users.noreply.github.com>
2022-05-06 09:15:07 +0200

    Update Program.cs

--- OOP/inheritance_intro/allatok/Program.cs
+++ OOP/inheritance_intro/allatok/Program.cs
@@ -7,20 +7,11 @@ namespace Allatok
     {
         static void Main(string[] args)
         {
-            Kutya bundas = new Kutya(5, 15);
-            Console.WriteLine("Kutya előtte, életkor: " + bundas.eletkor + ", testsúly: " + bundas.testsuly + ", életben van: " + bundas.eletbenVanE);
+            Kutya bundas = new Kutya(9,5,3,"Sanyi");
+            Console.WriteLine($"{bundas.gazda}, {bundas.eletkor}, {bundas.testsuly}, {bundas.szorosseg}, {bundas.eletbenVanE}");
             bundas.Eszik();
-            bundas.Oregszik();
+            Console.WriteLine($"{bundas.gazda}, {bundas.eletkor}, {bundas.testsuly}, {bundas.szorosseg}, {bundas.eletbenVanE}");
             Console.WriteLine(bundas.Ugat());
-            Console.WriteLine("Kutya utána, életkor: " + bundas.eletkor + ", testsúly: " + bundas.testsuly + ", életben van: " + bundas.eletbenVanE);
-
-            Macska cirmi = new Macska(2, 7);
-            Console.WriteLine("Macska előtte, életkor: " + cirmi.eletkor + ", testsúly: " + cirmi.testsuly + ", életben van: " + cirmi.eletbenVanE);
-            cirmi.Eszik();
-            cirmi.Oregszik();
-            Console.WriteLine(cirmi.Karmol());
-            cirmi.kill();
-            Console.WriteLine("Macska utána, életkor: " + cirmi.eletkor + ", testsúly: " + cirmi.testsuly + ", életben van: " + cirmi.eletbenVanE);
         }
     }
 }
