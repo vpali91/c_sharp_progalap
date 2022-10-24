874d6c875579be14fcc56ded38d82eeb68580490
vpali91 <44944542+vpali91@users.noreply.github.com>
2022-10-24 10:03:31 +0200

    Create Uszas.cs

---
+++ documentation/OOP/abstract_interface/Uszas.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AbstractInterface
+{
+    public interface Uszas
+    {
+        
+        public void Uszik();
+    }
+}
+
+// Elmélet: https://csharptutorial.hu/docs/hellovilag-hellocsharp/5-osztalyok-objektumok/interfeszek/
