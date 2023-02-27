34bd0477a15c0d132fbb37efb1e1e5dcc76e60c0
vpali91 <44944542+vpali91@users.noreply.github.com>
2023-02-27 12:15:21 +0100

    Create NyeroSzam.cs

---
+++ practice/lotto/NyeroSzam.cs
@@ -0,0 +1,20 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lotto
+{
+    class NyeroSzam
+    {
+        public int nySzam;
+        public int db;
+
+        public NyeroSzam(int nySzam, int db)
+        {
+            this.nySzam = nySzam;
+            this.db = db;
+        }
+    }
+}
